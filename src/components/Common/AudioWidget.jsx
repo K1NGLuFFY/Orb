0aedf70f817c1b911a5aa6d014e08b8a891af0bc
K1NGLuFFY <okekechiemerie123@gmail.com>
2026-07-31 12:20:25 +0100

    Fix UI issues and line-clamp CSS warnings

--- src/components/Common/AudioWidget.jsx
+++ src/components/Common/AudioWidget.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { Play, Pause, Volume2, VolumeX, Disc3 } from 'lucide-react';
 import { useAudio } from '../../context/AudioContext';
 import './AudioWidget.css';
 
 const AudioWidget = () => {
   const { isPlaying, isMuted, togglePlay, toggleMute } = useAudio();
+  const location = useLocation();
+
+  if (location.pathname.startsWith('/dashboard')) {
+    return null;
+  }
 
   return (
     <div className="audio-widget-container">
